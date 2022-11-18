031534666d32c15a222458bfccf692d4c788f83d
Shilpi21421 <S4shilpi567@gmail.com>
2022-11-18 11:27:12 +0530

    completed

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log('I love git');
+//console.log('I dont know what to do');
+// var h=document.getElementById('header-title');
+// h.innerText='Hello';
+// h.style.borderBottom='solid 3px black';
+// h.style.fontWeight='bold';
+var c=document.getElementsByClassName('list-group-item');
+c[1].innerText='Hello';
+c[1].style.background='yellow';
