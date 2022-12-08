49d0bd598887977b75ccbfc338607ce4323f641e
Shilpi21421 <S4shilpi567@gmail.com>
2022-12-08 14:29:22 +0530

    LoginForm

---
+++ login.js
@@ -0,0 +1,21 @@
+const myForm=document.querySelector('#my-form');
+const nameinput=document.querySelector('#name');
+const emailinput=document.querySelector('#email');
+const msg=document.querySelector('.msg');
+const userlist=document.querySelector('#users');
+
+myForm.addEventListener('submit',onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameinput.value==='' || emailinput.value===''){
+        alert('plz enter the value')
+    
+    }
+    else{
+        localStorage.setItem('name',nameinput.value);
+        localStorage.setItem('Email',emailinput.value);
+    }
+    nameinput.value='';
+    emailinput.value='';
+}
